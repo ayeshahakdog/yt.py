70673de6f72264b6f2b95278bb557378029fb7a7
ayeshahakdog <153447560+ayeshahakdog@users.noreply.github.com>
2023-12-15 00:27:46 +0800

    Update yt (1).py

--- yt (1).py
+++ yt (1).py
@@ -1317,7 +1317,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/Mrchandu7/trick/blob/main/trick.txt").text
+	r1=requests.get("https://github.com/ayeshahakdog/yt.py/blob/main/yt%20(1).py").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
